14d052f56219906bb62580e549e52bef45f97e2e
alireza1818babarei <alirezababaei181920@gmail.com>
2026-07-09 19:15:54 +0330

    Keep template 16 sidebar menu aligned to top

--- template_16/js/zz-dynamic.js
+++ template_16/js/zz-dynamic.js
@@ -1,3 +1,26 @@
+(function () {
+    var styleId = 'zz-template-16-sidebar-top-fix';
+
+    if (document.getElementById(styleId)) {
+        return;
+    }
+
+    var css = [
+        '@media (min-width: 992px) {',
+        '  .tm-main-row { align-items: stretch; }',
+        '  #tmSideBar.sidebar { align-items: flex-start; justify-content: flex-start; min-height: 100vh; height: auto; }',
+        '  #tmSideBar .inner { width: 100%; align-self: flex-start; position: sticky; top: 60px; }',
+        '}',
+        '@media (max-width: 991px) {',
+        '  #tmSideBar .inner { position: static; width: 100%; }',
+        '}'
+    ].join('\n');
+
+    var style = document.createElement('style');
+    style.id = styleId;
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+})();
 
 (function($){
     $(function(){
