6424938f310cfe1058f2645e72a45cde08544d39
alireza1818babarei <alirezababaei181920@gmail.com>
2026-07-10 22:50:34 +0330

    Load template 23 nav hover fix on services

--- template_23/service.php
+++ template_23/service.php
@@ -25,6 +25,7 @@ if (file_exists($bannerFile)) {
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css?id=<?php echo filemtime('assets/css/main.css'); ?>" />
+		<link rel="stylesheet" href="assets/css/zz-nav-hover-fix.css?id=<?php echo filemtime('assets/css/zz-nav-hover-fix.css'); ?>" />
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css?id=<?php echo filemtime('assets/css/noscript.css'); ?>" /></noscript>
 	</head>
 	<body class="template-page has-hero">
