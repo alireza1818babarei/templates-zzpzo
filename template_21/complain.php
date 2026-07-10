4b7c95147d456430e828233abd5102c9146cc9d4
alireza1818babarei <alirezababaei181920@gmail.com>
2026-07-10 21:45:22 +0330

    Load mobile footer fix on template 21 complain

--- template_21/complain.php
+++ template_21/complain.php
@@ -12,6 +12,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="stylesheet" href="assets/css/main.css?id=<?php echo filemtime('assets/css/main.css'); ?>" />
+        <link rel="stylesheet" href="assets/css/zz-mobile-footer.css?id=<?php echo filemtime('assets/css/zz-mobile-footer.css'); ?>" />
     </head>
     <body class="is-preload">
         <div id="page-wrapper">
