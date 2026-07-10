f21d0f7a54a502ccc9fe9a636c1dd97526eaef6a
alireza1818babarei <alirezababaei181920@gmail.com>
2026-07-10 23:11:02 +0330

    Load template 25 mobile layout fix on home

--- template_25/index.php
+++ template_25/index.php
@@ -20,6 +20,7 @@
     href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400&family=Playfair+Display:ital,wght@0,700;0,800;0,900;1,700&family=Instrument+Sans:wght@400;500;600&display=swap"
     rel="stylesheet">
   <link rel="stylesheet" href="templatemo-split-style.css?id=<?php echo filemtime('templatemo-split-style.css'); ?>">
+  <link rel="stylesheet" href="zz-mobile-layout.css?id=<?php echo filemtime('zz-mobile-layout.css'); ?>">
 </head>
 
 <body>
