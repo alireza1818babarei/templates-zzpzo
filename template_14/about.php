21d3f816fc46dc270190674768fda509f39bd8e1
alireza1818babarei <alirezababaei181920@gmail.com>
2026-07-11 16:00:54 +0330

    Load template 14 responsive fixes on about

--- template_14/about.php
+++ template_14/about.php
@@ -19,6 +19,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css?id=<?php echo filemtime('css/style.css'); ?>">
+  <link rel="stylesheet" href="css/zz-responsive-fixes.css?id=<?php echo filemtime('css/zz-responsive-fixes.css'); ?>">
 </head>
 
 <body>
