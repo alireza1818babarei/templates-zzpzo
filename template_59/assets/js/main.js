1876e9e1d787eb434843a0907cfd2bbbf6d24f3e
alireza18babaei <alireza18babaei@gmail.com>
2026-07-27 19:59:37 +0330

    m

--- template_59/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(function(link){
-  link.addEventListener('click', function(event){
-    var target = document.querySelector(this.getAttribute('href'));
-    if(target){ event.preventDefault(); target.scrollIntoView({behavior:'smooth'}); }
-  });
-});
